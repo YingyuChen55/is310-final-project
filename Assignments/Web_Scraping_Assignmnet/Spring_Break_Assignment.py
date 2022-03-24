2a16a510a0e5ba116d1a2df02f13198717adde50
YC <chenyingyu0505@outlook.com>
2022-03-23 22:13:50 -0500

    re-push the commented version

--- Assignments/Web_Scraping_Assignmnet/Spring_Break_Assignment.py
+++ Assignments/Web_Scraping_Assignmnet/Spring_Break_Assignment.py
@@ -1,9 +1,11 @@
+#First, Import all the important things.
 import requests
 import pathlib
-
+#Second, Open the text file 
 with open('/Users/YingyuChen/is310/Assignments/Web_Scraping_Assignmnet/raw_script_urls.txt',encoding = "ISO-8859-1") as file:
     text = file.readlines()
 
+#Third, Make a list of the dictionary that contains names and urls
 url_list = []
 
 for line in text:
@@ -14,6 +16,7 @@ for line in text:
     }
     url_list.append(info)
 
+#Fourth, Create the function to get response.text
 def read_remote(url):
   # assumes the url is already encoded (see urllib.parse.urlencode)
   with requests.get(url) as response:
@@ -21,12 +24,17 @@ def read_remote(url):
     if response.status_code == requests.codes.ok: # (that is 200)
       return response.text
   return None
-
+#Fifth, create a datafolder 
 datafolder = pathlib.Path('scripts_dump')
 if not datafolder.exists():
     datafolder.mkdir()
 
+#Sixth, loop over the url list, filter out 
+# the non-200 ones, 
+# the hundland -- which is the redirecting ones,
+# the too short ones -- that only contains the string that says work has been removed
 
+# Then write out the file with the book name as text file name and script as their content
 for item in url_list:
     response = requests.get(item['url'].strip())
     if response.status_code == 200:
